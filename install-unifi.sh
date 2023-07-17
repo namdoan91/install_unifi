e0853f44a46a078fcd48f627949b4cb72d991a60
namdoan91 <72986432+namdoan91@users.noreply.github.com>
2023-07-17 10:27:18 +0700

    Update install-unifi.sh
    if [ "$pkgname" == "openjdk8" ]; then ---> if [ "$pkgname" == "openjdk11" ]; then

--- install-unifi.sh
+++ install-unifi.sh
@@ -119,7 +119,7 @@ AddPkg () {
     env ASSUME_ALWAYS_YES=YES /usr/sbin/pkg add -f "$pkgurl" || exit 1
 
     # if update openjdk8 then force detele snappyjava to reinstall for new version of openjdk
-    if [ "$pkgname" == "openjdk8" ]; then
+    if [ "$pkgname" == "openjdk11" ]; then
       pkg unlock -yq snappyjava
       env ASSUME_ALWAYS_YES=YES /usr/sbin/pkg delete snappyjava
     fi
